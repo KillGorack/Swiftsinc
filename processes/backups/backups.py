8f6464c64d19b315a6098fda22feea1b9da4bb1a
David <davidkmonroe@outlook.com>
2023-09-30 08:56:48 -0400

    Backup Bugfix

--- processes/backups/backups.py
+++ processes/backups/backups.py
@@ -3,25 +3,18 @@ import shutil
 import os
 import time
 import schedule
-from multiprocessing import Process, Queue
 from datetime import datetime
+from multiprocessing import Queue
 
-
-def backup_directory(sourcepath, archivepath, name, queue):
+def backup_directory(sourcepath, archivepath, name, narchives, queue):
     if os.path.isdir(sourcepath):
         archivefile = os.path.join(
             archivepath, time.strftime("%Y%m%d-%H%M%S") + '.zip')
         if not os.path.exists(archivepath):
             os.makedirs(archivepath)
         shutil.make_archive(archivefile, 'zip', sourcepath)
-        queue.put({
-            'name': 'backups',
-            'status': 'OK',
-            'message': f'Backup of {name} completed.'
-        })
-
         archives = sorted(os.listdir(archivepath), reverse=True)
-        for archive in archives[2:]:
+        for archive in archives[narchives:]:
             os.remove(os.path.join(archivepath, archive))
 
         conn = sqlite3.connect('settings.db')
@@ -30,45 +23,49 @@ def backup_directory(sourcepath, archivepath, name, queue):
                   (datetime.now(), name))
         conn.commit()
         conn.close()
-
+        queue.put({
+            'name': 'backups',
+            'status': 'OK',
+            'message': f"Backup of {name} completed."
+        })
 
 def main(queue):
 
     queue.put({
         'name': 'backups',
         'status': 'OK',
-        'message': "Service started."
+        'message': f"Service started."
     })
 
-    conn = sqlite3.connect('settings.db')
-    c = conn.cursor()
-    c.execute("ALTER TABLE backups ADD COLUMN lastcomplete TIMESTAMP")
-    c.execute('SELECT * FROM backups')
-    rows = c.fetchall()
+    def daily_check():
+        conn = sqlite3.connect('settings.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM backups')
+        rows = c.fetchall()
 
-    for row in rows:
-        name = row[0]
-        sourcepath = row[1]
-        archivepath = row[2]
-        interval_days = row[3]
-        lastcomplete = row[4]
+        for row in rows:
+            name = row[0]
+            sourcepath = row[1]
+            archivepath = row[2]
+            interval_days = row[3]
+            lastcomplete = row[4]
+            narchives = row[5]
 
-        if not lastcomplete:
-            Process(target=backup_directory, args=(
-                sourcepath, archivepath, name, queue)).start()
-        else:
-            days_since_last_backup = (
-                datetime.now() - datetime.strptime(lastcomplete, "%Y-%m-%d %H:%M:%S")).days
-            if days_since_last_backup >= interval_days:
-                schedule.every(interval_days).days.do(Process(
-                    target=backup_directory, args=(sourcepath, archivepath, name, queue)).start())
+            if not lastcomplete:
+                backup_directory(sourcepath, archivepath, name, narchives, queue)
+            else:
+                days_since_last_backup = (
+                    datetime.now() - datetime.strptime(lastcomplete, "%Y-%m-%d %H:%M:%S.%f")).days
+                if days_since_last_backup >= interval_days:
+                    backup_directory(sourcepath, archivepath, name, narchives, queue)
+
+        conn.close()
+
+    schedule.every().day.at("00:10").do(daily_check)
 
     while True:
         schedule.run_pending()
-        time.sleep(60)
-
-    conn.close()
-
+        time.sleep(1)
 
 if __name__ == "__main__":
     queue = Queue()
