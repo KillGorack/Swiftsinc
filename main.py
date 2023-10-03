6d072b0fbfabb750782acba66ef3406b3fd94fae
David <davidkmonroe@outlook.com>
2023-10-03 18:53:22 -0400

    Dev

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ class Parent:
 
     def __init__(self):
 
-        logging.basicConfig(level=logging.INFO, filename='swiftsync.log', filemode='a',
-                            format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        logging.basicConfig(level=logging.INFO, filename='swiftsync.log', filemode='a', format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
         self.queue = Queue()
         self.processes = {}
@@ -35,20 +34,17 @@ class Parent:
 
         title_frame = tk.Frame(self.root)
         title_frame.pack(side='top', pady=10)
-        title_label = tk.Label(title_frame, text="Current Modules Loaded",
-                               font=self.textb, bg='black', fg='#cccccc')
+        title_label = tk.Label(title_frame, text="Current Modules Loaded", font=self.textb, bg='black', fg='#cccccc')
         title_label.pack()
 
         title_frame = tk.Frame(self.root)
         title_frame.pack(side='bottom', pady=10)
-        title_label = tk.Label(title_frame, text="Contact: dmonroe@killgorack.com",
-                               font=self.texta, bg='black', fg='#cccccc')
+        title_label = tk.Label(title_frame, text="Contact: dmonroe@killgorack.com", font=self.texta, bg='black', fg='#cccccc')
         title_label.pack()
 
         img = Image.open("swift.png")
         self.tk_img = ImageTk.PhotoImage(img)
-        label = tk.Label(self.root, image=self.tk_img,
-                         borderwidth=0, highlightthickness=0)
+        label = tk.Label(self.root, image=self.tk_img, borderwidth=0, highlightthickness=0)
         label.place(x=0, y=0)
 
         self.status_dot = {}
@@ -56,24 +52,18 @@ class Parent:
 
     def addService(self, process_name, row):
         try:
-            process_module = import_module(
-                f"processes.{process_name}.{process_name}")
+            process_module = import_module(f"processes.{process_name}.{process_name}")
             process_func = getattr(process_module, 'main')
-            self.processes[process_name] = Process(
-                target=process_func, args=(self.queue,))
+            self.processes[process_name] = Process(target=process_func, args=(self.queue,))
             service_frame = tk.Frame(self.root, bg='#cccccc')
             service_frame.pack(fill='x')
-            self.status_dot[process_name] = tk.Canvas(
-                service_frame, width=20, height=20)
+            self.status_dot[process_name] = tk.Canvas(service_frame, width=20, height=20)
             self.status_dot[process_name].grid(row=row, column=0)
-            self.status_dot[process_name].create_oval(
-                6, 7, 16, 17, fill='yellow')
-            self.labels[process_name] = tk.Label(
-                service_frame, text="", anchor='w', justify='left', font=self.texta, bg='#cccccc')
+            self.status_dot[process_name].create_oval(6, 7, 16, 17, fill='yellow')
+            self.labels[process_name] = tk.Label(service_frame, text="", anchor='w', justify='left', font=self.texta, bg='#cccccc')
             self.labels[process_name].grid(row=row, column=1, sticky='w')
         except ModuleNotFoundError:
-            logging.error(
-                f"Swiftsync: No module named 'processes.{process_name}.{process_name}'")
+            logging.error(f"Swiftsync: No module named 'processes.{process_name}.{process_name}'")
 
     def startServices(self):
         for process in self.processes.values():
@@ -107,7 +97,6 @@ class Parent:
                 self.status_dot[message['name']].itemconfig(1, fill='yellow')
                 logging.warn(f"{message['name']}: {message['message']}")
         self.root.after(1000, self.update_labels)
-
 
 if __name__ == "__main__":
     freeze_support()
